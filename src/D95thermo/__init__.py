e21afdd389348e788e047add438aaef0ce847fff
mdaeron <mathieu@daeron.fr>
2024-10-08 22:36:11 +0200

    Start adding docs

--- src/D95thermo/__init__.py
+++ src/D95thermo/__init__.py
@@ -6,8 +6,8 @@ __author__    = 'Mathieu Daëron'
 __contact__   = 'mathieu@daeron.fr'
 __copyright__ = 'Copyright (c) 2024 Mathieu Daëron'
 __license__   = 'MIT License - https://opensource.org/licenses/MIT'
-__date__      = '2024-10-01'
-__version__   = '0.1.0'
+__date__      = '2024-10-08'
+__version__   = '0.9.0'
 
 import numpy as _np
 import ogls as _ogls
