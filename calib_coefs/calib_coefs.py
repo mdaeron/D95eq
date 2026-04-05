97b92bac018d48ea385b044475e47de88c8ea9f7
mdaeron <mathieu@daeron.fr>
2026-04-05 15:01:53 +0200

    Fix coefs

--- calib_coefs/calib_coefs.py
+++ calib_coefs/calib_coefs.py
@@ -5,13 +5,16 @@ from D95thermo import _compute_D48_calib_coefficients
 
 
 coefs = Engine.D47_calib_coefs
-data = {
+data = correldata.CorrelData({
 	'degree' : _np.array([k for k,v in enumerate(coefs)]),
-	'coef': correldata.uarray(coefs)}
-correldata.save_data_to_file(data, 'D47_calib_coefs.csv', correl_format = 'z.9f')
+	'coef': correldata.uarray(coefs),
+})
+data.to_csv('D47_calib_coefs.csv', correl_format = 'z.9f')
+
 
 coefs = _compute_D48_calib_coefficients()
-data = {
+data = correldata.CorrelData({
 	'degree' : _np.array([k for k,v in enumerate(coefs)]),
-	'coef': correldata.uarray(coefs)}
-correldata.save_data_to_file(data, 'D48_calib_coefs.csv', correl_format = 'z.9f')
+	'coef': correldata.uarray(coefs),
+})
+data.to_csv('D48_calib_coefs.csv', correl_format = 'z.9f')
