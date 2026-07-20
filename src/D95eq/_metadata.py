2895c58b68186a353eb73f62a0d564311330f442
mdaeron <mathieu@daeron.fr>
2026-07-20 17:39:54 +0200

    Update metadata and docs

--- src/D95eq/_metadata.py
+++ src/D95eq/_metadata.py
@@ -1,8 +1,8 @@
-__version__     = "1.2.4"
+__version__     = "1.2.5"
 __description__ = "Test for clumped isotope equilibrium and estimate carbonate formation temperatures from dual clumped isotope measurements"
 __authors__     = "Mathieu Daëron & Timothy Pollard"
 __contact__     = "mathieu@daeron.fr"
 __license__     = "MIT"
 __copyright__   = "Mathieu Daëron"
-__date__        = "2026-06-10"
+__date__        = "2026-07-20"
 __all__         = ["__version__", "__description__", "__authors__", "__contact__", "__license__", "__copyright__", "__date__"]
